c6ea59f2b4944c212bde76e6d7d74301b7e417a9
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2022-04-15 16:21:21 +0300

    updated ticket.test.js

--- ticket.test.js
+++ ticket.test.js
@@ -21,13 +21,13 @@ describe("Ticket booking", () => {
     beforeEach(async () => {
         page = await browser.newPage();
         await page.goto("http://qamid.tmweb.ru/client/index.php", {
-            timeout: 60000
+            timeout: 60000,
         });
     });
     beforeAll(async () => {
         page = await browser.newPage();
         await page.goto("http://qamid.tmweb.ru/client/index.php", {
-            timeout: 60000
+            timeout: 60000,
         });
         await clickElement(page, daysWeek.secondDay);
         await clickElement(page, daysWeek.movi3Day);
@@ -46,9 +46,17 @@ describe("Ticket booking", () => {
         await clickElement(page, plases.row1PlaseVip);
         await clickElement(page, "button");
         await page.waitForSelector("h1");
-        const actual = await getText(page, "main > section > div > p:nth-child(2) > span", text => text.textContent);
+        const actual = await getText(
+            page,
+            "main > section > div > p:nth-child(2) > span",
+            (text) => text.textContent
+        );
         const expected = "1/2";
-        const actualPrise = await getText(page, "main > section > div > p:nth-child(6) > span", text => text.textContent);
+        const actualPrise = await getText(
+            page,
+            "main > section > div > p:nth-child(6) > span",
+            (text) => text.textContent
+        );
         const expectedPrise = "350";
         expect(actual).toContain(expected);
         expect(actualPrise).toContain(expectedPrise);
@@ -62,9 +70,17 @@ describe("Ticket booking", () => {
         await clickElement(page, plases.row5Plase6);
         await clickElement(page, "button");
         await page.waitForSelector("h1");
-        const actual = await getText(page, "main > section > div > p:nth-child(2) > span", text => text.textContent);
+        const actual = await getText(
+            page,
+            "main > section > div > p:nth-child(2) > span",
+            (text) => text.textContent
+        );
         const expected = "5/5, 5/6";
-        const actualPrise = await getText(page, "main > section > div > p:nth-child(6) > span", text => text.textContent);
+        const actualPrise = await getText(
+            page,
+            "main > section > div > p:nth-child(6) > span",
+            (text) => text.textContent
+        );
         const expectedPrise = "200";
         expect(actual).toContain(expected);
         expect(actualPrise).toContain(expectedPrise);
@@ -75,11 +91,8 @@ describe("Ticket booking", () => {
         await clickElement(page, daysWeek.movi3Day);
         await page.waitForSelector("h1");
         await clickElement(page, plases.row2Plase10);
-        const actual = await getText(page,
-            "h2",
-            (text) => text.textContent
-        );
+        const actual = await getText(page, "h2", (text) => text.textContent);
         const expected = "Фильм 3";
         expect(actual).toContain(expected);
     }, 60000);
-})
+});
